505b5cb7e2135a4b5b41da299a7dfb0bd7879930
SevgiChavushoglu <sevgi2308@gmail.com>
2023-12-21 11:10:31 +0200

    game win service added

--- src/api/controllers/roomController.ts
+++ src/api/controllers/roomController.ts
@@ -1,22 +1,43 @@
 import { Server, Socket } from "socket.io";
-import { OnMessage, SocketController, SocketIO, ConnectedSocket, MessageBody } from "socket-controllers";
+import {
+  OnMessage,
+  SocketController,
+  SocketIO,
+  ConnectedSocket,
+  MessageBody,
+} from "socket-controllers";
 
 @SocketController()
-export class RoomController{
+export class RoomController {
+  @OnMessage("join_game")
+  public async joinGame(
+    @SocketIO() io: Server,
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() message: any
+  ) {
+    console.log("Joining game", message);
+    const connectedSockets = io.sockets.adapter.rooms.get(message.roomId);
+    const socketRooms = Array.from(socket.rooms.values()).filter(
+      (r) => r !== socket.id
+    );
 
-    @OnMessage("join_game")
-    public async joinGame(@SocketIO() io: Server, @ConnectedSocket() socket: Socket, @MessageBody() message: any) {
-        console.log("Joining game", message);
-        const connectedSockets = io.sockets.adapter.rooms.get(message.roomId);
-        const socketRooms = Array.from(socket.rooms.values()).filter((r)=>r !== socket.id);
+    if (
+      socketRooms.length > 0 ||
+      (connectedSockets && connectedSockets.size == 2)
+    ) {
+      socket.emit("room_join_error", {
+        error: "Room is full",
+      });
+    } else {
+      await socket.join(message.roomId);
+      socket.emit("room_joined");
 
-        if (socketRooms.length > 0 || (connectedSockets && connectedSockets.size == 2)) {
-            socket.emit("room_join_error",{
-                error: "Room is full"
-            });
-        } else {
-            await socket.join(message.roomId);
-            socket.emit("room_joined");
-        }
+      if (io.sockets.adapter.rooms.get(message.roomId).size === 2) {
+        socket.emit("start_game", { start: true, symbol: "x" });
+        socket
+          .to(message.roomId)
+          .emit("start_game", { start: false, symbol: "o" });
+      }
     }
+  }
 }
