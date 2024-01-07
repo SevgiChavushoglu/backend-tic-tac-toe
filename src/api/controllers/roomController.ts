90ae99a7861377dffcd5d60de3709f1bfbef6cc2
SevgiChavushoglu <sevgi2308@gmail.com>
2024-01-07 16:12:29 +0200

    restart added

--- src/api/controllers/roomController.ts
+++ src/api/controllers/roomController.ts
@@ -7,6 +7,12 @@ import {
   MessageBody,
 } from "socket-controllers";
 
+interface RestartGameMessage {
+  ownState: "x" | "o";
+  winState: string;
+  roomId: string;
+}
+
 @SocketController()
 export class RoomController {
   @OnMessage("join_game")
@@ -37,6 +43,51 @@ export class RoomController {
         socket
           .to(message.roomId)
           .emit("start_game", { start: false, symbol: "o" });
+      }
+    }
+  }
+  // restart game
+  @OnMessage("restart_game")
+  public async restartGame(
+    @SocketIO() io: Server,
+    @ConnectedSocket() socket: Socket,
+    @MessageBody() message: RestartGameMessage
+  ) {
+    console.log("restarting game", message);
+    const winState = message.winState;
+    const ownState = message.ownState;
+
+    const getOppositeSymbol = (symbol: "x" | "o") => {
+      if (symbol == "x") {
+        return "o";
+      } else {
+        return "x";
+      }
+    };
+
+    const playerWon = () => {
+      return message.winState == message.ownState;
+    };
+
+    if (winState == "tie") {
+      socket.emit("start_game", {
+        start: true,
+        symbol: ownState,
+      });
+      socket.to(message.roomId).emit("start_game", {
+        start: false,
+        symbol: getOppositeSymbol(message.ownState),
+      });
+    } else {
+      if (playerWon()) {
+        socket.emit("start_game", {
+          start: true,
+          symbol: ownState,
+        });
+        socket.to(message.roomId).emit("start_game", {
+          start: false,
+          symbol: getOppositeSymbol(ownState),
+        });
       }
     }
   }
